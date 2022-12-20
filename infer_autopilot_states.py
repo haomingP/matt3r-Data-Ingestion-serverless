618124dd2b77916dfcb5ee7f6c2c83472e9d9c8f
haoming <haoming>
2022-12-20 11:18:32 -0800

    Modify lambda function to process sqs message

--- infer_autopilot_states.py
+++ infer_autopilot_states.py
@@ -39,70 +39,72 @@ def lambda_handler(event, context):
     # get log file from raw bucket
     body = event['Records'][0]['body']
     b = json.loads(body)
-    Message = b['Message']
-    mess = json.loads(Message)
-    object_key = urllib.parse.unquote_plus(mess['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    object_key = urllib.parse.unquote_plus(b['Records'][0]['s3']['object']['key'], encoding='utf-8')
 
-    # access parquet file
-    s3_path = "s3://" + RAW_BUCKET + '/' + object_key
-    df_parquet = wr.s3.read_parquet(path=s3_path)
+    event_bucket = b['Records'][0]['s3']['bucket']['name']
+    print(event_bucket)
+    s3_opt = b['Records'][0]['eventName']
+    if 'ObjectCreated' in s3_opt and RAW_BUCKET == event_bucket:
+        # access parquet file
+        s3_path = "s3://" + RAW_BUCKET + '/' + object_key
+        df_parquet = wr.s3.read_parquet(path=s3_path)
 
-    str = ''
-    land_dir = str.join(object_key.split('/')[0:-1])
-    land_bucket = s3_resource.Bucket(LANDING_BUCKET)
-    exsit_file_list = []
-    for object_summary in land_bucket.objects.filter(Prefix=land_dir):
-        exsit_file_list.append(object_summary.key)
+        str = ''
+        land_dir = str.join(object_key.split('/')[0:-1])
+        land_bucket = s3_resource.Bucket(LANDING_BUCKET)
+        exsit_file_list = []
+        for object_summary in land_bucket.objects.filter(Prefix=land_dir):
+            exsit_file_list.append(object_summary.key)
 
-    # filter autopilot
-    df_filtered = df_parquet[df_parquet['field'] == 'AutopilotState']
+        # filter autopilot
+        df_filtered = df_parquet[df_parquet['field'] == 'AutopilotState']
 
-    if not df_filtered.empty:
-        autopilot_df = pd.DataFrame()
-        for i in range(1, df_filtered.shape[0]):
-            if df_filtered.iloc[i, 3] == 3.0 and df_filtered.iloc[i - 1, 3] == 2.0:
-                new_df = pd.DataFrame(df_filtered.iloc[i]).T
-                new_df['Status'] = 'engagement'
-                autopilot_df = pd.concat([autopilot_df, new_df])
-            elif df_filtered.iloc[i, 3] <= 2.0 and df_filtered.iloc[i - 1, 3] == 3.0:
-                new_df = pd.DataFrame(df_filtered.iloc[i]).T
-                new_df['Status'] = 'disengagement'
-                autopilot_df = pd.concat([autopilot_df, new_df])
-            else:
-                pass
+        if not df_filtered.empty:
+            autopilot_df = pd.DataFrame()
+            for i in range(1, df_filtered.shape[0]):
+                if df_filtered.iloc[i, 3] == 3.0 and df_filtered.iloc[i - 1, 3] == 2.0:
+                    new_df = pd.DataFrame(df_filtered.iloc[i]).T
+                    new_df['Status'] = 'engagement'
+                    autopilot_df = pd.concat([autopilot_df, new_df])
+                elif df_filtered.iloc[i, 3] <= 2.0 and df_filtered.iloc[i - 1, 3] == 3.0:
+                    new_df = pd.DataFrame(df_filtered.iloc[i]).T
+                    new_df['Status'] = 'disengagement'
+                    autopilot_df = pd.concat([autopilot_df, new_df])
+                else:
+                    pass
 
-        # Convert to Json
-        autopilot_dict = {"auditory": {}}
-        for index, row in autopilot_df.iterrows():
-            tmp_dict = {}
-            tmp_dict[row['Status']] = [{"timestamp": row['timestamp'], "canbus_state": row['value']}]
-            autopilot_dict["auditory"].update(tmp_dict)
-        print(autopilot_dict)
+            # Convert to Json
+            autopilot_dict = {"auditory": {}}
+            for index, row in autopilot_df.iterrows():
+                tmp_dict = {}
+                tmp_dict[row['Status']] = [{"timestamp": row['timestamp'], "canbus_state": row['value']}]
+                autopilot_dict["auditory"].update(tmp_dict)
+            print(autopilot_dict)
 
-        # check if have engage / disengage in this day
-        if not autopilot_df.empty:
-            # check if need to combine
-            start_time_str = timestamp2string(df_filtered.iloc[0, 1])
-            start_date = (start_time_str.split(' ')[0]).split('/')[0]
-            start_month = (start_time_str.split(' ')[0]).split('/')[1]
-            start_year = (start_time_str.split(' ')[0]).split('/')[2]
-            filename = 'canserver-events_' + start_year + '-' + start_month + '-' + start_date + '.json'
-            path = land_dir + '/' + filename
-            if path in exsit_file_list:
-                last_file = wr.s3.read_json(path='s3://' + LANDING_BUCKET + '/' + path)
-                for k1 in last_file["auditory"].keys():
-                    for k2 in autopilot_dict["auditory"].keys():
-                        if k1 == k2:
-                            last_file["auditory"][k1].append(autopilot_dict["auditory"][k2])
-                autopilot_dict = last_file
+            # check if have engage / disengage in this day
+            if not autopilot_df.empty:
+                # check if need to combine
+                start_time_str = timestamp2string(df_filtered.iloc[0, 1])
+                start_date = (start_time_str.split(' ')[0]).split('/')[0]
+                start_month = (start_time_str.split(' ')[0]).split('/')[1]
+                start_year = (start_time_str.split(' ')[0]).split('/')[2]
+                filename = 'canserver-events_' + start_year + '-' + start_month + '-' + start_date + '.json'
+                path = land_dir + '/' + filename
+                if path in exsit_file_list:
+                    last_file = wr.s3.read_json(path='s3://' + LANDING_BUCKET + '/' + path)
+                    for k1 in last_file["auditory"].keys():
+                        for k2 in autopilot_dict["auditory"].keys():
+                            if k1 == k2:
+                                last_file["auditory"][k1].append(autopilot_dict["auditory"][k2])
+                    autopilot_dict = last_file
 
-            data_string = json.dumps(autopilot_dict, indent=2, default=str)
-            try:
-                s3.put_object(
-                    Body=data_string,
-                    Bucket=LANDING_BUCKET,
-                    Key=land_dir + '/' + filename
-                )
-            except Exception as e:
-                print(e)
-                print("writing to json failed!!!")
+                data_string = json.dumps(autopilot_dict, indent=2, default=str)
+                try:
+                    s3.put_object(
+                        Body=data_string,
+                        Bucket=LANDING_BUCKET,
+                        Key=land_dir + '/' + filename
+                    )
+                except Exception as e:
+                    print(e)
+                    print("writing to json failed!!!")
